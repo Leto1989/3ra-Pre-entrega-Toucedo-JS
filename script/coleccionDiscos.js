40671b83b6c711c84220ab11f0bc53eda827bdc0
Leandro Toucedo <turambar013@gmail.com>
2023-07-16 02:18:00 -0300

    Agreggué discos a la lista

--- script/coleccionDiscos.js
+++ script/coleccionDiscos.js
@@ -47,19 +47,53 @@ const discos = [
         lanzamiento: '1972', 
         genero: 'rock', 
         imagen: '../imagenes/Close to the Edge.png'
+    },
+    {
+        titulo: 'Highway to hell',
+        artista: 'ACDC',
+        lanzamiento: '1979',
+        genero: 'rock', 
+        imagen: 'imagenes/ac-dc-highway-to-hell-1-768x768.jpg',
+    }, 
+    {
+        titulo: 'Live at the Cheetah',
+        artista: 'Fania all Stars',
+        lanzamiento: '1971',
+        genero: 'salsa', 
+        imagen: '../imagenes/LiveatTheCheetah.jpg',
+    }, 
+    {
+        titulo: 'Ready to die', 
+        artista: 'Biggie Smalls', 
+        lanzamiento: '1994', 
+        genero: 'hip-hop', 
+        imagen: '../imagenes/ReadyToDie.jpg', 
+    }, 
+    {
+        titulo: 'Elis & Tom', 
+        artista: 'Elis Regina - Tom Jobim', 
+        lanzamiento: '1974', 
+        genero: 'bossa-nova', 
+        imagen: '../imagenes/Elis&tom.jpg', 
+    },
+    {
+        titulo: 'Expansions', 
+        artista: 'McCoy Tyner', 
+        lanzamiento: '1968', 
+        genero: 'Jazz', 
+        imagen: '../imagenes/McCoy-Tyner-Expansions-album-cover-web-optimised-820-brightness-1024x1024.jpg', 
     }
 ];
 
-//Función para mostrar los discos según el género seleccionado.
-
+// Función para mostrar los discos según el género seleccionado.
 function mostrarDiscosPorGenero (){
-    const generoSelector = document.getElementById ('selector-generos');
+    const generoSelector = document.getElementById ('selectorGeneros');
     const generoSeleccionado = generoSelector.value;
     const discosLista = document.getElementById ('discos-lista');
     discosLista.innerHTML = '';
     
     discos.forEach (function (disco) {
-        if (disco.genero ===generoSeleccionado){
+        if (disco.genero === generoSeleccionado){
             const tarjeta = document.createElement ('div');
             tarjeta.classList.add ('tarjeta');
 
@@ -86,28 +120,59 @@ function mostrarDiscosPorGenero (){
 
             tarjeta.appendChild (botonAgregar);
             discosLista.appendChild (tarjeta);
-            
-
         }
     });
-
 }
 
-//Función para agregar un disco a la lista personal
-
+// Función para agregar un disco a la lista personal
 function agregarDiscoPersonal (disco) {
     const nuevoElemento = document.createElement ('li');
     nuevoElemento.textContent = disco.titulo + ' - ' + disco.artista;
     const listaPersonal = document.getElementById ('lista-personal');
     listaPersonal.appendChild (nuevoElemento);
-
 }
 
 
-// Asociar el evento onChange del selector de género a la función mostrarDiscosPorGenero
-document.getElementById('selector-generos').addEventListener('change', mostrarDiscosPorGenero);
-
-// Mostrar los discos iniciales al cargar la página
-mostrarDiscosPorGenero();
+// Obtener elementos del DOM después de que se haya cargado el HTML
+document.addEventListener("DOMContentLoaded", function () {
+    const btnAgregarDisco = document.getElementById("btnAgregarDisco");
+    const formularioAgregar = document.getElementById("formularioAgregar");
+    const formulario = document.getElementById("formularioAgregarDisco");
+    const selectorGeneros = document.getElementById('selectorGeneros');
+  
+    // Muestra el formulario al hacer clic en el botón
+    btnAgregarDisco.addEventListener("click", function () {
+      formularioAgregar.style.display = "block";
+    });
+  
+    formulario.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const titulo = document.getElementById("titulo").value;
+      const artista = document.getElementById("artista").value;
+      const lanzamiento = document.getElementById("lanzamiento").value;
+      const genero = document.getElementById("genero").value;
+      const imagen = document.getElementById("imagen").value;
+  
+      const nuevoDisco = {
+        titulo: titulo,
+        artista: artista,
+        lanzamiento: lanzamiento,
+        genero: genero,
+        imagen: imagen,
+      };
+  
+      discos.push(nuevoDisco);
+  
+      formulario.reset();
+      formularioAgregar.style.display = "none";
+      mostrarDiscosPorGenero();
+    });
+    
+    //Asocio el evento ONchange del selector de género a la función mostrarDiscos por género
+    selectorGeneros.addEventListener ("change", mostrarDiscosPorGenero)
+    // Mostrar los discos iniciales al cargar la página
+    mostrarDiscosPorGenero();
+  });
 
 
